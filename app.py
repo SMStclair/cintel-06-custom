fbb95da796a6a1ef433303cae17b4fe5df9636dc
SMStclair <156228968+SMStclair@users.noreply.github.com>
2024-04-16 02:00:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import pandas as pd
 
 #-----------------------------------------------------------------------------
-# define a reactive calc to filter the life expectancy seaborn dataset
+# load csv into app.py
 #-----------------------------------------------------------------------------
 lec_df: pd.DataFrame  = pd.read_csv(Path(__file__).parent / "example.csv")
 
@@ -91,11 +91,6 @@ with ui.navset_card_tab(id="tab"):
 # --------------------------------------------------------
 # Reactive calculations and effects
 # --------------------------------------------------------
-
-# Add a reactive calculation to filter the data
-# By decorating the function with @reactive, we can use the function to filter the data
-# The function will be called whenever an input functions used to generate that output changes.
-# Any output that depends on the reactive function (e.g., lec_df()) will be updated when the data changes.
 
 @reactive.calc
 def dat():
